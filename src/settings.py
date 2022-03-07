6142f5b6129586ca30dad426676fe9378915db62
aadilkadiwal <aadilkadiwal4277@gmail.com>
2022-03-07 22:02:49 +0530

    Added css and logo

--- src/settings.py
+++ src/settings.py
@@ -130,6 +130,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'src/static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
