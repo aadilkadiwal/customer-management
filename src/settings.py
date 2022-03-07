1956febfaf945019463c11c5d5a2396940123e1e
aadilkadiwal <aadilkadiwal4277@gmail.com>
2022-03-07 20:19:13 +0530

    Dashboard, Customer and Products Template

--- src/settings.py
+++ src/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import Csv, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_extensions',
+    # Custom apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +60,10 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            #Template application loaction
+            os.path.join(BASE_DIR, 'src/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
