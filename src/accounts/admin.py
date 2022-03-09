1290ed517743f7e61a058042f47e6ae03674c9b2
aadilkadiwal <aadilkadiwal4277@gmail.com>
2022-03-09 15:45:21 +0530

    Added Customization Admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,30 @@
+from re import search
 from django.contrib import admin
+from .models import (
+    Customer,
+    Tag,
+    Product,
+    Order
+)
 
-# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email',)
+    search_fields = ('name', 'phone', 'email',)
+admin.site.register(Customer, CustomerAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)     
+admin.site.register(Tag, TagAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price',)
+    list_filter = ('category', 'tags',)
+    search_fields = ('name',)
+admin.site.register(Product, ProductAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'product', 'status',)
+    list_filter = ('status',)
+    search_fields = ('customer', 'product',)
+admin.site.register(Order, OrderAdmin)
